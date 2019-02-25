1c6e9ca1ce38ea4aa1352ee18ea141c6ce280870
irakli svanidze <irakli1393@gmail.com>
2019-02-25 16:00:40 +0400

    fixed gitlab issues

--- src/bigcommerce/dynamic-tab.vue
+++ src/bigcommerce/dynamic-tab.vue
@@ -1,109 +1,115 @@
 <template>
-  <div id="dynamicTab">
-    <div class="tab-header">
-      <button
-        v-for="tab in tabs"
-        @click="onTabClick(tab)"
-        class="tab-button"
-        :class="{ active: tab.visible }"
-      >
-        {{ tab.key }}
-      </button>
-    </div>
+    <div id="dynamicTab">
+        <div class="tab-header">
+            <button v-for="tab in tabs" @click="onTabClick(tab)" class="tab-button" :class="{ active: tab.visible }">
+                {{ tab.key }}
+            </button>
+        </div>
 
-    <div
-      :ref="tab.key"
-      :id="tab.key"
-      v-for="tab in tabs"
-      v-if="tab.visible"
-      class="tab-body"
-      :class="{ active: tab.visible }"
-    >
-      <div v-html="tab.description"></div>
+        <div :ref="tab.key" :id="tab.key" v-for="tab in tabs" v-if="tab.visible" class="tab-body" :class="{ active: tab.visible }">
+            <div v-html="tab.description"></div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import helper from './dynamic-tab-helpers';
-  export default {
-    data() {
-      return {
-        tabs: [],
-        currentTab: null
-      };
-    },
-    created() {
-      this.parseDescription(this.description);
-      this.injectTabs(this.selectorsToInject);
-      this.activateFirstTab();
-    },
-    methods: {
-      onTabClick(tab) {
-        if (tab.visible === true && tab.key === this.currentTab.key) return;
-        tab.visible = true;
-        if (this.currentTab) {
-          this.currentTab.visible = false;
-        }
-        this.currentTab = tab;
-      },
-      parseDescription(description) {
-        const parsedTabs = helper.parseDescription(description);
-        this.tabs = this.tabs.concat(parsedTabs);
-        if (this.tabs.length === 0) {
-          this.tabs.push({
-            key: this.defaultTabName,
-            description,
-            visible: true
-          });
-        }
-      },
-      injectTabs(selectors) {
-        selectors.forEach(item => {
-          const el = document.querySelector(item.selector);
-          if (el) {
-            this.tabs.push({
-              key: item.tabTitle,
-              description: el.innerHTML,
-              visible: false
-            });
-          }
-        });
-      },
-      activateFirstTab() {
-        if (this.tabs.length > 0) {
-          const activeTab = this.tabs[0];
-          activeTab.visible = true;
-          this.currentTab = activeTab;
-        }
-      }
-    },
-    props: {
-      description: {
-        type: String,
-        required: true
-      },
-      selectorsToInject: {
-        type: Array,
-        required: false
-      },
-      defaultTabName: {
-        type: String,
-        required: true
-      }
-    }
-  };
+    import helper from './helpers';
+    export default {
+        data() {
+            return {
+                tabs: [],
+                currentTab: null,
+            };
+        },
+        created() {
+            this.parseDescription(this.description);
+            this.injectTabs(this.selectorsToInject);
+            this.activateFirstTab();
+        },
+        methods: {
+            /**
+             * make tab active when user will click on that
+             * @param {*} tab
+             *
+             */
+            onTabClick(tab) {
+                if (tab.visible === true && tab.key === this.currentTab.key) return;
+                tab.visible = true;
+                if (this.currentTab) {
+                    this.currentTab.visible = false;
+                }
+                this.currentTab = tab;
+            },
+            /**
+             * pars description to extract tabs
+             * catch all edgecage and render full descrion
+             * as html if something goes wrong
+             * @param {*} description
+             */
+            parseDescription(description) {
+                try {
+                    const parsedTabs = helper.parseDescription(description);
+                    this.tabs = this.tabs.concat(parsedTabs);
+                } catch (error) {
+                    this.tabs.push({
+                        key: this.defaultTabName,
+                        description,
+                        visible: true,
+                    });
+                }
+            },
+            /**
+             * Inject additional tab based on selector.
+             * @param {*} selector
+             */
+            injectTabs(selectors) {
+                selectors.forEach(item => {
+                    const el = document.querySelector(item.selector);
+                    if (el) {
+                        this.tabs.push({
+                            key: item.tabTitle,
+                            description: el.innerHTML,
+                            visible: false,
+                        });
+                    }
+                });
+            },
+            /**
+             * activate first tab in tabs array. set visible prop - true
+            */
+            activateFirstTab() {
+                if (this.tabs.length === 0) return;
+                const activeTab = this.tabs[0];
+                activeTab.visible = true;
+                this.currentTab = activeTab;
+            },
+        },
+        props: {
+            description: {
+                type: String,
+                required: true,
+            },
+            selectorsToInject: {
+                type: Array,
+                required: false,
+            },
+            defaultTabName: {
+                type: String,
+                required: true,
+            },
+        },
+    };
 </script>
 
 <style lang="scss">
-  .tab-header {
-    background-color: black;
-    color: white;
-  }
-  .tab-button {
-    padding: 15px;
-  }
-  .tab-body {
-    padding: 15px;
-  }
+    .tab-header {
+        background-color: black;
+        color: white;
+    }
+    .tab-button {
+        padding: 15px;
+    }
+    .tab-body {
+        padding: 15px;
+    }
 </style>
---
+++ src/bigcommerce/helpers.js
@@ -0,0 +1,29 @@
+const helpers = {
+    /**
+     * parsing description and extractiong tabs
+     * @param {*} description
+     * @returns array of parsed tabs
+     */
+    parseDescription(description) {
+        const tabs = [];
+        const el = document.createElement('body');
+        el.innerHTML = description;
+        const rows = el.querySelectorAll('body table tbody tr');
+        if (!rows) {
+            throw new Error("Invalid Table. Couldn't Parse Description Data");
+        }
+        rows.forEach(r => {
+            const cells = r.querySelectorAll('td');
+            if (cells.length !== 2) {
+                throw new Error('Invalid Table. Invalid Number of Columns. Table Should Have 2 Columns');
+            }
+            tabs.push({
+                key: cells[0].innerHTML,
+                description: cells[1].innerHTML,
+                visible: false,
+            });
+        });
+        return tabs;
+    },
+};
+export default helpers;

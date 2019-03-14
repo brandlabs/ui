ecafe33e8929b274c40855ba93d5a835e112f144
irakli svanidze <irakli1393@gmail.com>
2019-03-14 15:44:50 +0400

    fix package

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './index.js',
     module: {
         rules: [
             {

e22d3208b2ce9aa5c486c7592a6c9c137dba4171
irakli svanidze <irakli1393@gmail.com>
2019-03-14 15:18:34 +0400

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import dynamicTab from './bigcommerce/dynamic-tab.vue';
-import specsTable from './bigcommerce/specs-table.vue';
-
 const something = {
     blah() {
         console.log('QQQQQQQQQQQQ');

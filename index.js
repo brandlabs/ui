e52e6e973b258d30d30a8a0786787483aea5ad56
irakli svanidze <irakli1393@gmail.com>
2019-03-18 18:42:26 +0400

    fix exports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import dynamicTabs from './src/bigcommerce/dynamic-tab.vue';
-import specsTable from './src/bigcommerce/specs-table.vue';
+import DynamicTabs from './src/bigcommerce/dynamic-tab.vue';
+import SpecsTable from './src/bigcommerce/specs-table.vue';
 
 export default {
     BigCommerce: {
         DynamicTabs,
-        SpecsTable
-    }
+        SpecsTable,
+    },
 };
